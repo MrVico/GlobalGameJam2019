ff14187378097b7ebac51556b0532c7d2576436a
Mehdi-Ben <mehdibenlamine@hotmail.fr>
2019-01-26 01:09:21 +0100

    GameManager mise en place startPosition/endPosition
    GameManager mise en place startPosition/endPosition

---
+++ Assets/Scripts/FinishScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishScript : MonoBehaviour
+{
+    public bool collideWithPlayer;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collideWithPlayer = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            inverseCollision();
+        }
+        Debug.Log(collision.collider.tag);
+    }
+
+    public void inverseCollision()
+    {
+        collideWithPlayer = !collideWithPlayer;
+    }
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject startPosition;
+    public GameObject finishPosition;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = GameObject.FindGameObjectWithTag("StartPosition");
+        finishPosition = GameObject.FindGameObjectWithTag("FinishPosition");
+        spawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finishPosition.GetComponent<FinishScript>().collideWithPlayer)
+        {
+            spawnPlayer();
+            finishPosition.GetComponent<FinishScript>().inverseCollision();
+        }
+    }
+
+    private void spawnPlayer()
+    {
+        player.position = startPosition.transform.position;
+    }
+}
