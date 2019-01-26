65666c5ebcd8952e3203f1f70dd8fdd8afa6cf00
Mehdi-Ben <mehdibenlamine@hotmail.fr>
2019-01-26 03:24:07 +0100

    Ajout Main Camera en prefab
    Ajout Main Camera en prefab à utiliser pour le suivi de joueur. Ajouter gameobject Player dans la target. Parametrer les valeurs min et max de la cam.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Levier activator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activator.activated)
+        {
+            ActivateDoor();
+        }
+    }
+
+    public void ActivateDoor()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿// Smooth towards the target
+
+using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3F;
+    public float posY;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, posY, -10));
+
+        // Smoothly move the camera towards that target position
+        Vector3 desiredPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        transform.position = new Vector3(Mathf.Clamp(desiredPosition.x, minX, maxX), Mathf.Clamp(desiredPosition.y, minY, maxY), desiredPosition.z);
+    }
+}
