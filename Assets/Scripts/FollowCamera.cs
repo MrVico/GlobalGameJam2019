98576875df1b7c418cd836ec96afc4869f06a83e
MrVico <zarakikov67@gmail.com>
2019-01-26 17:10:51 +0100

    Changed camera movement

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,6 +6,11 @@ using System.Collections;
 public class FollowCamera : MonoBehaviour
 {
     public Transform target;
+    public float dampTime = 0.15f;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 verticalOffset = new Vector3(0f, 1.5f, 0f);
+    /*
     public float smoothTime = 0.3F;
     public float posY;
     public float minX;
@@ -13,7 +18,6 @@ public class FollowCamera : MonoBehaviour
     public float minY;
     public float maxY;
 
-    private Vector3 velocity = Vector3.zero;
 
     void Update()
     {
@@ -23,5 +27,17 @@ public class FollowCamera : MonoBehaviour
         // Smoothly move the camera towards that target position
         Vector3 desiredPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         transform.position = new Vector3(Mathf.Clamp(desiredPosition.x, minX, maxX), Mathf.Clamp(desiredPosition.y, minY, maxY), desiredPosition.z);
+    }
+    */
+
+    // Update is called once per frame
+    void Update() {
+        if (target) {
+            Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 destination = transform.position + delta + verticalOffset;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+
     }
 }
