0012bc6b13bb47a1d433d5e8d4f20bc3ea69cd8b
David Fumey <fumey.david@hotmail.com>
2019-01-27 03:33:43 +0100

    lvl2

---
+++ Assets/GottaGoFast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GottaGoFast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Equals("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 45;
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag.Equals("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+        }
+    }
+}
---
+++ Assets/SpecialZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("PlayerCharacter"))
+        {
+            collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("PlayerCharacter"))
+        {
+            collision.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
